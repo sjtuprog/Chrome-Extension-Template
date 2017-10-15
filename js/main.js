ee891d5f75b8d4dded49543fe8114ae28f0f5ff3
Lei Gao <sjtuprog@gmail.com>
2017-10-14 22:54:04 -0500

    First Commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,25 @@
 
 function init_main () {
     $('html').hide().fadeIn('slow');
+    
+
+
+    
 }
 
 //bind events to dom elements
+
+console.log('aaa');
+
 document.addEventListener('DOMContentLoaded', init_main);
+
+//replace css
+var cssLink = $('link[href*="jobsearch_all.css"]');
+cssLink.replaceWith('<link href="../css/template.css" type="text/css" rel="stylesheet">');
+
+
+// replace images with cats
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
